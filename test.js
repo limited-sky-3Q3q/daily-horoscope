1c1eb966d889bec53c69f9c25e5d98212d8f1f1a
SR <2060204880@qq.com>
2026-03-09 21:59:55 +0800

    "fix"

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ const horoscope = generateHoroscope(new Date());
 // 格式化消息
 const message = formatHoroscopeMessage(horoscope);
 
-// 发送消息
-await sendMessage(message, ['console', 'log']);
+// 发送消息（包含微信推送）
+await sendMessage(message, ['console', 'log', 'wechat']);
 
 console.log('✅ 测试完成! 运行 `npm start` 启动定时服务');
